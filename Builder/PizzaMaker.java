e1adebc5f147554652a696b0bfe77526ffdd4742
Brianee <bnguerrero@cpp.edu>
2023-03-24 01:07:36 -0700

    final edits

--- Builder/PizzaMaker.java
+++ Builder/PizzaMaker.java
@@ -4,23 +4,48 @@ public class PizzaMaker
 {
     public static void main(String[] args) {
 
-        String chain = "Pizza Hut";
+        String chain; 
         String size;
         String toppings; 
+        int choice = 0;
 
         Scanner scan = new Scanner(System.in);
+        
+        do
+        {
+            System.out.println("Hello would you like to order from Pizza Hut, Little Caesars, or Dominos ? \n");
+            chain = scan.nextLine();
+    
+            System.out.println("Welcome to " + chain );
+    
+            System.out.println("Please select a size: small, medium, or large\n");
+            size = scan.nextLine();
+                
+            System.out.println("Please select toppings: \nPepperoni, Sausage, Mushrooms, Bacon," +" \nOnions, Extra Cheese, Peppers, Chicken," + "\nOlives, Spinach, Tomato and Basil, Beef\n");
+            toppings = scan.nextLine();
+    
+            Pizza pizza = new PizzaBuilder().setChain(chain).setSize(size).setToppings(toppings).build();
+    
+            pizza.eat();
 
-        System.out.println("Welcome to " + chain );
+            System.out.println("\nWould you like to make another order?\n1. Yes\n2. No");
+            choice = scan.nextInt();
+        
+            switch(choice)
+            {
+                case 1:
+                    choice = 1;
+                    break;
 
-        System.out.println(" Please select a size: small, medium, or large");
-        size = scan.nextLine();
-            
-        System.out.println("Please select toppings: \nPepperoni, Sausage, Mushrooms, Bacon" +" \nOnions, Extra Cheese, Peppers, Chicken" + "\nOlives, Spinach, Tomato and Basil, Beef");
-        toppings = scan.nextLine();
-
-        Pizza pizza = new PizzaBuilder().setChain(chain).setSize(size).setToppings(toppings).build();
-
-        pizza.eat();
+                case 2: 
+                    break;
+                default:
+                    System.out.println("invalid input");
+                    break;
+            }
+        }
+        while(choice != 2);
+        
         
         scan.close();
     }
